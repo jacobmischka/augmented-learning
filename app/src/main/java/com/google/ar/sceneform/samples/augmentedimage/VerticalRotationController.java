656ca29a174281408ad2a8d0b477fdc0b4c69c98
Jacob Mischka <jacob@mischka.me>
2018-11-29 22:06:34 -0600

    It finally rotates vertically.
    Not really in the way that I want it to, but it's good enough for now.

--- app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/VerticalRotationController.java
+++ app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/VerticalRotationController.java
@@ -1,15 +1,18 @@
 package com.google.ar.sceneform.samples.augmentedimage;
 
+import android.support.v4.math.MathUtils;
+import android.util.Log;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.math.MathHelper;
+import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.ux.BaseTransformationController;
 import com.google.ar.sceneform.ux.TransformableNode;
+import com.google.ar.schemas.lull.Quat;
 
 public class VerticalRotationController extends BaseTransformationController<TwoFingerDragGesture> {
     private final TransformableNode node;
-    private static final float DELTA_MULTIPLIER = -0.0002f;
-    private static final float MAX_HEIGHT = 0.25f;
+    private static final float DELTA_MULTIPLIER = 1f;
     public VerticalRotationController(
             TransformableNode transformableNode, TwoFingerDragGestureRecognizer gestureRecognizer) {
         super(transformableNode, gestureRecognizer);
@@ -21,11 +24,12 @@ public class VerticalRotationController extends BaseTransformationController<Two
     }
     @Override
     public void onContinueTransformation(TwoFingerDragGesture gesture) {
-        Vector3 localPos = node.getLocalPosition();
-        float newY = localPos.y + (gesture.getAverageDeltaPosition().y * DELTA_MULTIPLIER);
-        newY = MathHelper.clamp(newY, 0.0f, MAX_HEIGHT);
-        localPos.y = newY;
-        node.setLocalPosition(localPos);
+        float deltaY = gesture.getAverageDeltaPosition().y * DELTA_MULTIPLIER;
+        Log.d("VerticalRotationController", String.valueOf(deltaY));
+        MathUtils.clamp(deltaY, -360, 360);
+        Quaternion localRotation = new Quaternion(new Vector3(1f, 0f, 0f), deltaY);
+        Log.d("VerticalRotationController", localRotation.toString());
+        node.setLocalRotation(Quaternion.multiply(node.getLocalRotation(), localRotation));
     }
     @Override
     public void onEndTransformation(TwoFingerDragGesture gesture) {
